4ec92fbe92d53d1aa358175a29a74424c9ed4900
Pablo Seijo <pgarsei510@gmail.com>
2024-02-29 11:57:58 +0100

    General Reestructuring and corrections

--- accesos.sh
+++ accesos.sh
@@ -45,99 +45,35 @@ opcion="$1"
 case $opcion in
   "-c")
 
-    ### -------------------------- Opción C ------------------------------ 
+    ### -------------------------- Opción -c ------------------------------ 
+
     echo "Has seleccionado la opción -c."
 
-    # Divide el archivo en doce partes, con un prefijo para los nombres de archivo, para poder procesarlos en paralelo
-    split -n 12 "$2" archivo_parte_
+    >resultados.txt # Crea un archivo vacío llamado resultados.txt, o si ya existe, lo vacía.
 
-    # Crear un archivo temporal para almacenar los resultados intermedios
-    temp_file=$(mktemp)
-
-    ### ----------- Procesamiento cada archivo en paralelo -------------- ###
-
-    
-    # Para esto se utiliza el comando xargs, que permite nos permite la ejecución de comandos en paralelo. A traves de la opción -P 12, se le 
-    #indica que ejecute 12 comandos en paralelo, que es el numero de archivos que se generaron con el comando split. por otra parte {} bash 
-    #-c, indica que se ejecute el comando bash -c, con el argumento {} que es el archivo que se le pasa a xargs.
-
-    ls archivo_parte_* | xargs -P 12 -I {} bash -c 'while IFS= read -r linea; do
-        
-        respuesta=$(echo "$linea" | awk "{print \$9}")
-        echo "$respuesta"
-
-    done < "{}"' >> "$temp_file"
-
-    # Filtra los resultados únicos y los guarda en 'resultados.txt', luego elimina el archivo temporal
-    sort -u "$temp_file" > resultados.txt
-    rm "$temp_file"
-
-    # Limpieza: elimina los archivos temporales de las partes
-    rm archivo_parte_*
-
-    # Mostramos los resultados
-    while IFS= read -r linea; do
-        echo "$linea"
-    done < "resultados.txt"
-
-
-    # Limpieza: elimina el archivo temporal de resultados
-    rm resultados.txt
+    # Utilizamos awk para coger el penultimo numero de la linea, que es el codigo de respuesta, y lo imprimimos por pantalla.
+    awk '{print $(NF-1)}' log/access.log | sort -u
 
     echo "Operación -c realizada con éxito."
 
     ;;
-  "GET/POST")
+  "GET" | "POST")
 
     
     ### -------------------------- Opción GET/POST ------------------------------ ###
 
     echo "Has seleccionado la opción GET/POST."
 
-    >resultados.txt # Crea un archivo vacío llamado resultados.txt, o si ya existe, lo vacía.
-
-    # Divide el archivo en doce partes, con un prefijo para los nombres de archivo, para poder procesarlos en paralelo. $2 es el archivo que se le pasa al script. archivo_parte_ es el 
-    # prefijo que se le da a los archivos que se generan.
-    split -n 12 "$2" archivo_parte_
-
-    # Crear un archivo temporal para almacenar los resultados intermedios
-    temp_file=$(mktemp)
-
-    ### ----------- Procesamiento de cada archivo en paralelo -------------- ###
-    
-    # Para esto se utiliza el comando xargs, que nos permite la ejecución de comandos en paralelo. A traves de la opción -P 12, se le 
-    # indica que ejecute 12 comandos en paralelo, que es el numero de archivos que se generaron con el comando split. por otra parte {} bash 
-    # GET/POST, indica que se ejecute el comando bash GET/POST, con el argumento {} que es el archivo que se le pasa a xargs.
-    ls archivo_parte_* | xargs -s 10000 -P 12 -I {} bash -c 'while IFS= read -r linea; do
-        
-        getPost=$(echo "$linea" | awk "{sub(/^\"/, \"\", \$6); print \$6}")
-        respuesta=$(echo "$linea" | awk "{print \$9}")
-
-        # Si el campo getPost es igual a GET o POST y el campo respuesta es igual a 200, se imprime en el archivo resultados.txt
-        if [[ ("$getPost" == "GET" || "$getPost" == "POST") && "$respuesta" == "200" ]]; then
-      echo "$getPost $respuesta" >> "resultados.txt"
+    # Miramos el codigo de la opcion para seleccionar GET o POST
+    if ["$opcion" == "GET"]; then
+        # Si la opcion es GET, utilizamos awk para seleccionar las lineas que contienen GET y 200, y las contamos.
+        contador=$(awk '$6 == "\"GET\"" && $(NF-1) == 200' log/access.log | wc -l)
+    else
+        # Si la opcion es POST, utilizamos awk para seleccionar las lineas que contienen POST y 200, y las contamos.
+        contador=$(awk '$6 == "\"POST" && $(NF-1) == 200' log/access.log | wc -l)
     fi
 
-    done < "{}"' >> "$temp_file"
-
-
-    # Limpieza: elimina los archivos temporales de las partes
-    rm archivo_parte_*
-
-    contador=0;
-
-    # Por cada linea incrementamos el contador en uno
-    while IFS= read -r linea; do
-        let contador++ 
-    done < "resultados.txt"
-
-    # Limpieza: elimina el archivo temporal de resultados
-    # rm resultados.txt
-
-    # Mostramos los resultados
-    # date "+%b %d %T" muestra la fecha y hora actual en el formato especificado
-    # $contador muestra el valor de la variable contador
-    echo "$(date "+%b %d %T"). Registrados $contador accesos tipo GET/POST con respuesta 200."
+    echo "$(date "+%b %d %T"). Registrados $contador accesos tipo $opcion con respuesta 200."
 
 
     ;;
@@ -147,109 +83,69 @@ case $opcion in
     echo "Has seleccionado la opción -s."
 
     # Inicializamos los contadores, uno para los bytes y otro para las lineas
-    contadorBytes=0;
-    contadorLineas=0;
-
-    # Divide el archivo en doce partes, con un prefijo para los nombres de archivo, para poder procesarlos en paralelo. $2 es el archivo que se le pasa al script. archivo_parte_ es el 
-    # prefijo que se le da a los archivos que se generan.
-    split -n 12 "$2" archivo_parte_
-
-    # Crear un archivo temporal para almacenar los resultados intermedios
-    temp_file=$(mktemp)
-
-    ### ----------- Procesamiento de cada archivo en paralelo -------------- ###
+    contadorBytes=0
+    contadorLineas=0
     
-    # Para esto se utiliza el comando xargs, que nos permite la ejecución de comandos en paralelo. A traves de la opción -P 12, se le dice que ejecute 12 comandos en paralelo, que es el numero de archivos que se generaron con el comando split.
-    # A diferencia de los anteriores podemos ver que dentro de cada comando se ejecuta un bucle que recorre cada linea del archivo que se le paga con xargs, cogiendo las varbiables y sumando los bytes.
-    # Una notable consideraciión es que se utiliza la comprobación =~ ^[0-9]+$. Esto significa que se comprueba si el valor de bytes es un número entero positivo, pues =~ es un operador de coincidencia de expresiones regulares, despues ^[0-9]+$ es una expresión regular que comprueba si el valor de bytes es un número entero positivo.
-    # Una expresión regular es una secuencia de caracteres que forma un patrón de búsqueda. Cuando se busca datos en un texto, puede utilizar este patrón para describir lo que está buscando. Estas pueden transformarse en automatas finitos deterministas, que son una forma de representar un lenguaje regular.
-    ls archivo_parte_* | xargs -P 12 -I {} bash -c '{
-        localBytes=0
-        localLines=0 # Inicializa el contador de líneas
-        while IFS=" " read -r ip client user datetime1 datetime2 method url protocol status bytes
-        do
-            # Asegura que solo se sumen valores numéricos para los bytes
-            if [[ "$bytes" =~ ^[0-9]+$ ]]; then
-                ((localBytes+=bytes))
-            fi
-            ((localLines++)) # Incrementa el contador de líneas por cada línea procesada
-        done < "{}"
-        echo "$localBytes $localLines" # Imprime tanto los bytes como las líneas contadas
-    }' >> "$temp_file"
+    # Utilizamos awk para sumar los bytes de todas las lineas del archivo, y lo almacenamos en la variable contadorBytes. Basicamente suma el ultimo 
+    # campo de cada linea y cuando termina imprime el resultado en la variable contadorBytes.
+    contadorBytes=$(awk '{contadorBytes+=$NF} END {print contadorBytes}' log/access.log)
 
+    # Convertimos los bytes a KiB
+    contadorBytes=$((contadorBytes/1024))
 
-    # Limpieza: elimina los archivos temporales de las partes
-    rm archivo_parte_*
-
-    # Leer el archivo temporal para sumar los bytes totales
-    while IFS= read -r linea; do
-
-        # Extraemos del temp file los bytes
-        bytes=$(echo "$linea" | awk "{print \$1}")
-        lineas=$(echo "$linea" | awk "{print \$2}")
-
-        #Sumamos los bytes y contamos las lineas
-        ((contadorBytes+=bytes))
-        let contadorLineas+=$lineas;
-
-    done < "$temp_file"
-
-    # Eliminar el archivo temporal
-    rm "$temp_file"
-
-    # Convertir el total de bytes a KiB
-    contadorBytesKiB=$((contadorBytes/1024))
+    # Utilizamos wc -l para contar las lineas del archivo, y lo almacenamos en la variable contadorLineas.
+    contadorLineas=$(wc -l < log/access.log)
 
     # Mostramos los resultados
     echo "$contadorBytes KiB enviados en $contadorLineas peticiones."
-
-
 
     ;;
   "-t")
 
     ### -------------------------- Opción -t ------------------------------ ###
 
-    # TODO: Implementar la opción -t
+    #TODO: Implementar la opción -t 
 
     echo "Has seleccionado la opción -t."
 
-    >resultados.txt # Crea un archivo vacío llamado resultados.txt, o si ya existe, lo vacía.
+    # Extraer las fechas de primer y último acceso del archivo access.log, utilizamos sed para elimnar el corchete y cut para quedarnos con la fecha, con las opciones -d y -f indicamos el delimitador y el campo que queremos.
+    primerAcceso=$(awk 'NR==1 {print $4}' log/access.log | sed 's/\[//' | cut -d: -f1)
+    ultimoAcceso=$(awk 'END {print $4}' log/access.log | sed 's/\[//' | cut -d: -f1)
 
-    # Divide el archivo en doce partes, con un prefijo para los nombres de archivo, para poder procesarlos en paralelo. $2 es el archivo que se le pasa al script. archivo_parte_ es el 
-    # prefijo que se le da a los archivos que se generan.
-    split -n 12 "$2" archivo_parte_
+    # Definir los días de cada mes (asumiendo que todos los años son no bisiestos)
+    dias_por_mes=(0 31 28 31 30 31 30 31 31 30 31 30 31)
 
-    # Crear un archivo temporal para almacenar los resultados intermedios
-    temp_file=$(mktemp)
+    # Calcular la cantidad de días transcurridos desde el 1 de enero de 1
+    dias_desde_inicio_ano() {
+        local dia=$1
+        local mes=$2
+        local ano=$3
+        local dias=$((ano * 365 + dia))
+        for ((m = 1; m < mes; m++)); do
+            dias=$((dias + dias_por_mes[m]))
+        done
+        echo $dias
+    }
 
-    ### ----------- Procesamiento de cada archivo en paralelo -------------- ###
-    
+    # Calcular la cantidad de días transcurridos desde el 1 de enero de 1 hasta la fecha
+    dias_fecha1=$(dias_desde_inicio_ano ${fecha1[@]})
+    dias_fecha2=$(dias_desde_inicio_ano ${fecha2[@]})
 
-    ls archivo_parte_* | xargs -P 12 -I {} bash -c '{
-        while IFS=" " read -r ip client user datetime1 datetime2 method url protocol status bytes
-        do
-            # Asumiendo que queremos procesar `datetime1` que contiene la fecha y hora
-            fecha_original=$datetime1
-            # Eliminar el corchete y reemplazar el primer ':' por un espacio para separar la fecha de la hora
-            fecha_ajustada=$(echo "$fecha_original" | sed -e "s/\[/ /" -e "s/:/ /")
+    echo "Días desde el 1 de enero de 1 hasta la fecha 1: $dias_fecha1"
 
-            # Usar `date` con el formato ajustado
-            fecha_formateada=$(date -d "$fecha_ajustada" "+%Y-%m-%d %H:%M:%S" 2>/dev/null)
-            echo "$fecha_formateada" # Imprime la fecha formateada
-        done < "{}"
-    }' >> "$temp_file"
+    # Calcular la diferencia en días entre las dos fechas
+    diferencia_dias=$((dias_fecha2 - dias_fecha1))
 
-    sort -u "$temp_file" > resultados.txt
+    echo "Días totales: $diasTotales"
+
+    # Contar los días únicos con accesos
+    diasConAcceso=$(awk '{print $4}' log/access.log | sed 's/\[//' | cut -d: -f1 | sort -u | wc -l)
+
+    # Calcular los días sin acceso
+    diasSinAcceso=$((diasTotales - diasConAcceso))
 
 
-    # Limpieza: elimina los archivos temporales de las partes
-    rm archivo_parte_*
-
-    # Limpieza: elimina el archivo temporal de resultados
-    rm "$temp_file"
-
-    echo "Operación -t realizada con éxito."
+    echo "Días sin acceso: $diasSinAcceso"
 
     ;;  
   *)
