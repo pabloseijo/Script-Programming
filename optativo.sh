d2783fc1de23b373458cf8bc47a0039a0e225051
Pablo Seijo <pgarsei510@gmail.com>
2024-03-13 11:17:11 +0100

    Bugs fixing

--- optativo.sh
+++ optativo.sh
@@ -12,15 +12,16 @@ echo "ID Simulación | Tiempo de Vuelo [fs] | Corriente [A]" > datos_filtrados.t
 
 # Inicializamos una variable para almacenar la suma de las corrientes
 sumaCorrientes=0
+
 # Inicializamos un contador para el número de simulaciones
 contador=0
 
-# Usamos un bucle para procesar cada archivo de resultados
+# Usamos un bucle para procesar cada archivo de resultados ($SIMULATION_PATH/*.txt coge todos los archivos del directorio)
 for file in ${SIMULATION_PATH}/*; do
     # Extraemos el ID de la simulación del nombre del archivo
     id=$(basename "$file" .txt)
 
-    # Buscamos la línea con el tiempo de vuelo deseado y extraemos la corriente
+    # Buscamos la línea con el tiempo de vuelo deseado y extraemos la corriente (la opción -v permite pasar una variable a awk)
     corriente=$(awk -v time="1.355000e+03" '$1==time {print $12}' "$file")
 
     # Mostramos por pantalla la corriente asociada a la simulación
@@ -31,14 +32,12 @@ for file in ${SIMULATION_PATH}/*; do
 
     # Acumulamos la corriente y aumentamos el contador
     sumaCorrientes=$(echo "$sumaCorrientes + $corriente" | bc)
+
     ((contador++))
 done
 
-# Calculamos la corriente media
-if [ $count -gt 0 ]; then
-    avg_current=$(echo "scale=9; $sum_current / $contador" | bc)
-    # Imprimimos por pantalla la corriente media
-    echo "> El valor medio de la corriente, una vez evaluadas $contador simulaciones es: ${avg_current} A"
-else
-    echo "No se encontraron archivos de simulación para procesar."
-fi
+# Calculamos la corriente media 
+corrienteMedia=$(echo "scale=9; $sumaCorrientes / $contador" | bc -l)
+
+# Imprimimos por pantalla la corriente media
+echo "> El valor medio de la corriente, una vez evaluadas $contador simulaciones es: ${corrienteMedia} A"
